8e32a9f6f146ec71a43846bcdaf697f57acde7d0
Shishir-Sap <shishirsapkota@msn.com>
2023-01-11 19:20:09 +0100

    change port no

--- Flask_project/flask_app.py
+++ Flask_project/flask_app.py
@@ -1,17 +1,14 @@
-# Add to this file for the microweb app lab
 from flask import Flask
 from flask import request
 from flask import render_template
+import datetime
 
 microweb_app = Flask(__name__)
 
-
 @microweb_app.route("/")
 def main():
-    return render_template("index.html")
-
+    return render_template("index.html" , datetime_now = datetime.datetime.now())
 
 if __name__ == "__main__":
     microweb_app.run(host="0.0.0.0", port=5050)
-
 # sdsds
